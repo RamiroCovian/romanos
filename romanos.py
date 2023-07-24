058f445b2ca494f646d91e89899f03fca231aefc
RamiroCovian <ramiro.covian@gmail.com>
2023-07-24 17:43:20 -0300

    feat: validacion numero entre 1 y 3999

--- romanos.py
+++ romanos.py
@@ -3,7 +3,7 @@ Escribir una funcion en python que sea capaz de recibir un numero entre 0 y 3999
 y devuelva el numero como una cadena en su representacion en numero romano.
 
 *Restriccion*
-El numero 0 devolvera una cadena vacia.
+El numero 0 no es un numero romano valido.
 1. definir una funcion
 2. La funcion recibe un parametro
 3. El parametro es numerico
@@ -16,31 +16,17 @@ El numero 0 devolvera una cadena vacia.
 def convertir_a_romano(numero):
     if type(numero) != int:
         return "ERROR: debes ingresar un valor numerico"
-    elif type(numero) == int:
-        return "TODO: convertir a romano"
-
-    # if not isinstance(numero, int):
-    #     return "ERROR: debes ingresar un valor numerico"
-    # return "TODO: convertir a romano"
-
-
-# def comprobar_entero(numero):
-#     try:
-#         num = int(numero)
-#         return "TODO: convertir a romano"
-#     except ValueError:
-#         print(f"Error: debes introducion un valor entero ({numero})")
-#         return ""
+    # validar el valor del numero
+    if not (0 < numero < 4000):
+        return f"Error: El numero debe estar entre 1 y 3999 ({numero})"
+    return "TODO: convertir a romano"
 
 
 print(convertir_a_romano(56))
 print(convertir_a_romano("lo que quiera"))
-print(convertir_a_romano(51.3))
+print(convertir_a_romano(0))
+print(convertir_a_romano(4000))
+print(convertir_a_romano(-6))
+print(convertir_a_romano(3999))
 print(convertir_a_romano({}))
 print(convertir_a_romano([]))
-# print("---" * 21)
-# print(comprobar_entero(56))
-# print(comprobar_entero("lo que quiera"))
-# print(comprobar_entero(51.3))
-# print(comprobar_entero({}))
-# print(comprobar_entero([]))
